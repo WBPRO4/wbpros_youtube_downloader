9e9d901e7cc1017d7ec52661242315ffebe2acc5
WBPRO4 <bazsiwulfinger@gmail.com>
2023-08-18 12:27:13 +0200

    added file mode

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 mod structs;
 use structs::ParsedArgs;
+use std::{io::{self, BufRead}, process::exit};
 use sanitize_filename::sanitize;
-use std::{fs, env, process::Command};
+use std::{fs, env, path::Path, process::Command};
 use rusty_ytdl::{Video, VideoInfo, VideoOptions, VideoQuality, VideoSearchOptions};
 
 #[tokio::main]
@@ -10,11 +11,33 @@ async fn main() {
 
     let args: ParsedArgs = parse_args(env::args().collect());
 
-    if args.filemode{} // TODO:implement later
+    if args.filemode
+    {
+      // File hosts.txt must exist in the current path
+      if let Ok(lines) = read_download_list(args.filename.to_string()) 
+      {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines 
+        {
+          if let Ok(url) = line 
+          {
+            dbg!(&url);
+            let (video, video_info) = get_video_info(&url, &args).await;
+            download_video(&video, &video_info, &args).await;
+            println!("Moving onto next link in list...")
+          }
+        }
+        println!("Done!\nExiting!");
+        exit(0) 
+      }
+    }
+    else
+    {
+      let (video, video_info) = get_video_info(&args.url, &args).await;
+      download_video(&video, &video_info, &args).await;
+      println!("Done!")
+    }
 
-    let (video, video_info) = get_video_info(&args.url, &args).await;
-    download_video(&video, &video_info, &args).await;
-    println!("Done!")
 }
 
 async fn get_video_info(url: &String, args: &ParsedArgs) -> (rusty_ytdl::Video, VideoInfo)
@@ -52,7 +75,7 @@ async fn download_video(video: &Video, video_info: &VideoInfo, args: &ParsedArgs
     // do this thrice cuz command line args
     if args.as_m4a{clean_tmp_file_title = format!("{}.m4a", sanitized_title);}
     // create a path object
-    let tmp_file_path: &std::path::Path = std::path::Path::new(clean_tmp_file_title.as_str());
+    let tmp_file_path: &Path = Path::new(clean_tmp_file_title.as_str());
     // download video
     video.download(tmp_file_path).await.unwrap();
     
@@ -87,19 +110,39 @@ fn parse_args(input_args: Vec<String>) -> ParsedArgs
   {
     url: "".to_string(),
     filemode: false,
+    filename: "".to_string(),
     with_video: false,
     video_only: false,
     as_m4a: false
   };
+
   for (iter, arg) in input_args.iter().enumerate()
   {
     if iter == 0 {continue;}                         // should be the executable name
-    if iter == 1 {parsed_args.url = arg.to_string()} // should be the link or id
-    
-    if arg == "--file" {parsed_args.filemode = true;}  // we are in file mode
+    if iter == 2 && parsed_args.filemode {parsed_args.filename = arg.to_string()} // if filemode was specified earlier than filename shoud be here
+    if iter == 1 
+    {
+      if arg == "--file" // if we are in filemode
+      {
+        parsed_args.filemode = true; // set the switch
+        println!("Running in file mode");
+        continue; // and terminate iteration early we dont need to check for a url
+      } 
+      else 
+      {    
+        parsed_args.url = arg.to_string()
+      }
+    } // should be the link or id or --file to specify filemode
+
     if arg == "--with-video" {parsed_args.with_video = true;} // we are downloading MP4s
     if arg == "--video-only" {parsed_args.video_only = true;} // only download the video not the audio
     if arg == "--as-m4a" {parsed_args.as_m4a = true;} // dont perform conversion to MP3
   }
   return parsed_args;
+}
+
+fn read_download_list<P>(filename: P) -> io::Result<io::Lines<io::BufReader<fs::File>>>
+where P: AsRef<Path>, {
+    let file = fs::File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
--- src/structs.rs
+++ src/structs.rs
@@ -2,7 +2,9 @@ pub struct ParsedArgs
 {
   pub url: String,
   pub filemode :bool,
+  pub filename :String,
   pub with_video :bool,
   pub video_only :bool,
   pub as_m4a :bool
+
 }
